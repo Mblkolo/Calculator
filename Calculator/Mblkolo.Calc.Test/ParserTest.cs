41318fafaca5b1393bc2722e76e1c7c27b51fcde
Mblikolo <rusakov-rv@yandex.ru>
2016-02-21 00:32:28 +0500

    Файл не добавился в чекин, добавил

---
+++ Calculator/Mblkolo.Calc.Test/ParserTest.cs
@@ -0,0 +1,42 @@
+﻿using Mblkolo.Calc.Commands;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mblkolo.Calc.Test
+{
+    [TestFixture]
+    public class ParserTest
+    {
+        //Самый сложный вопрос: где же остановиться?
+        //Вычислитель выражений может поодерживать
+        //Лево и право ассоциативные операторы
+        //Унарные операторы, операторы из нескольких символов
+        //Всякие там неопределённые поведения i++ + ++i
+        //Поэтому делаем несколько допущений
+        //1. Все операторы односимвольные
+        //2. В выражении содержатся только числа, операторы и пробелы с табами в качестве разделителей
+        //3. Унарные операторы только префиксные и правоассоциативные
+        //4. Никаких фунаций (но должно быть расширяемо)
+
+        //Выражение - это [число] или [(выражение)] или [выражение бинарный оператор выражение] или [унарный оператор выражение]
+
+        [Test]
+        public void ExpressionIsValue()
+        {
+            var parser = new Parser();
+
+            Stack<ICommand> commands= parser.Parse("1");
+
+            Assert.That(commands.Count, Is.EqualTo(1));
+            var command = commands.Peek();
+            Assert.That(command, Is.TypeOf<PushCommand>());
+            Assert.That((command as PushCommand).Value, Is.EqualTo(1m));
+        }
+
+
+    }
+}
