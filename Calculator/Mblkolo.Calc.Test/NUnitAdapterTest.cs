22279406ca0386ab22cff27159a8eb659a5a50ed
Mblikolo <rusakov-rv@yandex.ru>
2016-02-20 21:08:02 +0500

    Добавил nUnitAdapter, чтобы запускать тесты в VS

---
+++ Calculator/Mblkolo.Calc.Test/NUnitAdapterTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mblkolo.Calc.Test
+{
+    [TestFixture]
+    public class NUnitAdapterTest
+    {
+        [Test]
+        public void Run()
+        {
+            Assert.That(true);
+        }
+
+    }
+}
