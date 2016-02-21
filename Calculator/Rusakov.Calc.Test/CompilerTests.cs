6b37be6802aa72ec1a900eb1a36703f805f124c4
Mblikolo <rusakov-rv@yandex.ru>
2016-02-21 19:55:02 +0500

    Реализовал поддержку скобок и чисел

---
+++ Calculator/Rusakov.Calc.Test/CompilerTests.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+using Rusakov.Calc.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rusakov.Calc.Test
+{
+    [TestFixture]
+    class CompilerTests
+    {
+        [Test]
+        public void ComlipeNumber()
+        {
+            var compiler = new Compiler();
+            var lexemes = new[] { new Lexeme("23.2", LexemeType.Number) };
+
+            var commands = compiler.Compile(lexemes);
+
+            Assert.That(commands.Length, Is.EqualTo(1));
+            Assert.That(commands[0], Is.TypeOf<PushCommand>());
+            Assert.That((commands[0] as PushCommand).Value, Is.EqualTo(23.2m));
+        }
+
+        [Test]
+        public void FailComlipeNumber()
+        {
+            var compiler = new Compiler();
+            var lexemes = new[] { new Lexeme("23..2", LexemeType.Number) };
+
+            TestDelegate commands = () => compiler.Compile(lexemes);
+
+            Assert.Throws<ArgumentException>(commands);
+        }
+
+        [Test]
+        public void ComlipeBrackets()
+        {
+            var compiler = new Compiler();
+            var lexemes = new[] { 
+                new Lexeme("(", LexemeType.OpenBracket),
+                new Lexeme("1", LexemeType.Number),
+                new Lexeme(")", LexemeType.CloseBracket),
+            };
+
+            var commands = compiler.Compile(lexemes);
+
+            Assert.That(commands.Length, Is.EqualTo(1));
+            Assert.That(commands[0], Is.TypeOf<PushCommand>());
+            Assert.That((commands[0] as PushCommand).Value, Is.EqualTo(1m));
+        }
+
+        [Test]
+        public void EmptyBracketsNotAllowed()
+        {
+            var compiler = new Compiler();
+            var lexemes = new[] { 
+                new Lexeme("(", LexemeType.OpenBracket),
+                new Lexeme(")", LexemeType.CloseBracket)
+            };
+            TestDelegate commands = () => compiler.Compile(lexemes);
+
+            Assert.Throws<ArgumentException>(commands);
+        }
+
+        [Test]
+        public void MultiplyBracketsNotAllowed()
+        {
+            var compiler = new Compiler();
+            var lexemes = new[] { 
+                new Lexeme("(", LexemeType.OpenBracket),
+                new Lexeme("(", LexemeType.OpenBracket),
+                new Lexeme("1", LexemeType.Number),
+                new Lexeme(")", LexemeType.CloseBracket),
+                new Lexeme(")", LexemeType.CloseBracket)
+            };
+
+            TestDelegate commands = () => compiler.Compile(lexemes);
+
+            Assert.Throws<ArgumentException>(commands);
+        }
+
+        [Test]
+        public void NotBalansedCloseBrackets()
+        {
+            var compiler = new Compiler();
+            var lexemes = new[] { 
+                new Lexeme("(", LexemeType.OpenBracket),
+                new Lexeme("1", LexemeType.Number),
+                new Lexeme(")", LexemeType.CloseBracket),
+                new Lexeme(")", LexemeType.CloseBracket)
+            };
+
+            TestDelegate commands = () => compiler.Compile(lexemes);
+
+            Assert.Throws<ArgumentException>(commands);
+        }
+
+        [Test]
+        public void NotBalansedOpenBrackets()
+        {
+            var compiler = new Compiler();
+            var lexemes = new[] { 
+                new Lexeme("(", LexemeType.OpenBracket),
+                new Lexeme("(", LexemeType.OpenBracket),
+                new Lexeme("1", LexemeType.Number),
+                new Lexeme(")", LexemeType.CloseBracket)
+            };
+
+            TestDelegate commands = () => compiler.Compile(lexemes);
+
+            Assert.Throws<ArgumentException>(commands);
+        }
+    }
+}
--- Calculator/Rusakov.Calc/Compiler.cs
+++ Calculator/Rusakov.Calc/Compiler.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Rusakov.Calc.Commands;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +13,56 @@ namespace Rusakov.Calc
         public ICommand[] Compile(Lexeme[] lexemes)
         {
             var commands = new List<ICommand>();
+            var lexStack = new Stack<Lexeme>();
 
-            for(int i=0; i<lexemes.Length; ++i)
+            foreach(var lex in lexemes)
             {
+                if(lex.Type == LexemeType.Number)
+                {
+                    decimal number;
+                    if(!decimal.TryParse(lex.Value, NumberStyles.Any, CultureInfo.InvariantCulture, out number))
+                        throw new ArgumentException(String.Format("Не удалось преобразовать в число '{0}'", lex.Value));
 
+                    commands.Add(new PushCommand(number));
+                    continue;
+                }
+                if(lex.Type == LexemeType.OpenBracket)
+                {
+                    lexStack.Push(lex);
+                    continue;
+                }
+                if(lex.Type == LexemeType.CloseBracket)
+                {
+                    try
+                    {
+                        for (var l = lexStack.Pop(); l.Type != LexemeType.OpenBracket; l = lexStack.Pop() )
+                        {
+                            //Что делаем
+                        }
+                    }
+                    catch(InvalidOperationException)
+                    {
+                        throw new ArgumentException("Обнаружена непарная закрывающая скобка");
+                    }
+
+                    continue;
+                }
+
+                if(lex.Type == LexemeType.Operator)
+                {
+                    //На самом деле нужно ещё доставать лишнее
+                    lexStack.Push(lex);
+                    continue;
+                }
+            }
+
+            while(lexStack.Count > 0)
+            {
+                var lex = lexStack.Pop();
+                if(lex.Type == LexemeType.OpenBracket)
+                    throw new ArgumentException("Обнаружена непарная открывающая скобка");
+
+                //Перекидываем операции в результат
             }
 
             return commands.ToArray();
