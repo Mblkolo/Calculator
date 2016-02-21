981e289e0a0d1546ba9349ccb9203ca2a5ca866c
Mblikolo <rusakov-rv@yandex.ru>
2016-02-21 19:01:47 +0500

    Написал тесты для компиллера

---
+++ Calculator/Rusakov.Calc/Compiler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rusakov.Calc
+{
+    class Compiler
+    {
+        public ICommand[] Compile(Lexeme[] lexemes)
+        {
+            var commands = new List<ICommand>();
+
+            for(int i=0; i<lexemes.Length; ++i)
+            {
+
+            }
+
+            return commands.ToArray();
+        }
+    }
+}
