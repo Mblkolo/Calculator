9dfca2a54d3f3bf04188e2b8d489b74521209556
Mblikolo <rusakov-rv@yandex.ru>
2016-02-23 20:13:23 +0500

    Добавил приёмочные тесты, перед тем как добавлять поддержку унарных операций

---
+++ Calculator/Rusakov.Calc.Test/CalculatorAcceptanceTests.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using Rusakov.Calc.Interfaces;
+using Rusakov.Calc.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rusakov.Calc.Test
+{
+    [TestFixture]
+    class CalculatorAcceptanceTests
+    {
+        private Calculator _calc;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            var lexer = new Lexer();
+            var operations = new IOperation[] 
+            {
+                new PlusOperation(),
+                new MinusOperation(),
+                new MultiplyOperation(),
+                new DivideOperation()
+            };
+
+            var compiler = new Compiler(operations);
+            _calc = new Calculator(lexer, compiler);
+        }
+
+        public static object[] CorrectExpressionSource = 
+        {
+            new object[] { "5", 5m },
+            new object[] { "5+2", 7m },
+            new object[] { "5-2", 3m },
+            new object[] { "5*2", 10m },
+            new object[] { "5/2", 2.5m },
+            new object[] { "(((3)))", 3m },
+            new object[] { "()5()*()2()", 10m }, //Это странно, да
+            new object[] { "1 + 2 * 3", 7m },
+            new object[] { "1 * 2 + 3", 5m },
+            new object[] { "3 * 2 / 3", 2m },
+            new object[] { "4 / 2 / 2", 1m },
+        };
+
+        [Test, TestCaseSource("CorrectExpressionSource")]
+        public void CorrectExpression(string expression, decimal answer)
+        {
+            decimal result = _calc.Calculate(expression);
+
+            Assert.That(result, Is.EqualTo(answer));
+        }
+
+        [TestCase("")]
+        [TestCase("  \t")]
+        [TestCase("(")]
+        [TestCase(")")]
+        [TestCase("(()")]
+        [TestCase("())")]
+        [TestCase("+")]
+        [TestCase("10 20")]
+        [TestCase("1..34")]
+        [TestCase("34 -")]
+        [TestCase("1/0")]
+        [TestCase("1/(1-1)")]
+        [TestCase("-34")]
+        public void IncorrectExpression(string expression)
+        {
+            TestDelegate action = () => _calc.Calculate(expression);
+
+            Assert.Throws<CalculationException>(action);
+        }
+    }
+}
